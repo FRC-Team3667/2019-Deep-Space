9ed4962c4a279cf39c2526146a3d9154806930f1
Phil Hudson <33849969+TimeReaderLLC@users.noreply.github.com>
2019-02-02 10:44:16 -0500

    2/2 Checkin

--- Robot.java
+++ Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -56,7 +49,7 @@ public class Robot extends IterativeRobot {
   SpeedControllerGroup rightMotors = null;
   DifferentialDrive _dDrive = null;
   MecanumDrive _mDrive = null;
-  Joystick _drive = new Joystick(0);
+  Joystick _drive = null;
   private UsbCamera camera;
 
   // 10 Degrees of Freedom
@@ -92,6 +85,17 @@ public class Robot extends IterativeRobot {
    */
   @Override
   public void robotInit() {
+    // Setup the joystick
+    try {
+      _drive = new Joystick(0);
+    } catch (Exception e0) {
+      try {
+        _drive = new Joystick(1);
+      } catch (Exception e1) {
+      }
+    }
+
+    // Setup the Drive System
     if (mDrive) {
       _frontVLeftMotor = new WPI_VictorSPX(13);
       _frontVRightMotor = new WPI_VictorSPX(12);
